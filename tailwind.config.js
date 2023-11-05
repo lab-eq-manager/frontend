fa887640a5c729a6780671283b1a68585ea7df4c
WitchElaina <msz13399173387@gmail.com>
2023-11-05 18:28:02 +0800

    Fix tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,10 @@ import { nextui } from '@nextui-org/react';
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    './index.html',
     // Next UI use this
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{tsx,ts,jsx,js}',
   ],
   theme: {
     extend: {},
