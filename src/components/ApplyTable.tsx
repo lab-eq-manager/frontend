4cd983adc12b92a8a8e6bd9304fc3740d32d5d17
WitchElaina <msz13399173387@gmail.com>
2023-11-09 23:51:08 +0800

    Add apply table

--- src/components/ApplyTable.tsx
+++ src/components/ApplyTable.tsx
@@ -5,77 +5,106 @@ import {
   TableCell,
   TableHeader,
   TableColumn,
-  Dropdown,
-  DropdownTrigger,
-  DropdownMenu,
-  DropdownSection,
-  DropdownItem,
+  Popover,
   Button,
-  Chip,
+  CheckboxGroup,
+  Checkbox,
+  PopoverTrigger,
+  PopoverContent,
+  Selection,
 } from '@nextui-org/react';
 import { mockApplyEquipmentRequests } from '@/utils/mockData';
-import { availableTime } from '@/types';
+import { ApplyEquipmentRequest, availableTime } from '@/types';
 import { VerticalDotsIcon } from '@/assets/VerticalDotsIcon';
+import { useCallback, useState, useMemo } from 'react';
 
-const tabelHeaders = [
-  '申请人',
-  '申请器材',
-  '使用日期',
-  '使用时段',
-  '申请时间',
-  '状态',
-  '操作',
-] as const;
+const columns = [
+  { name: '申请人', uid: 'uid' },
+  { name: '设备编号', uid: 'eqId' },
+  { name: '申请时间', uid: 'applyTime' },
+  { name: '使用日期', uid: 'applyDate' },
+  { name: '使用时段', uid: 'timeIndex' },
+  { name: '申请状态', uid: 'status' },
+  { name: '操作', uid: 'actions' },
+];
+
+const INITIAL_VISIBLE_COLUMNS = ['uid', 'eqId', 'applyDate', 'timeIndex', 'actions'];
 
 export const ApplyTable = () => {
+  const [visibleColumns, setVisibleColumns] = useState<Selection>(new Set(INITIAL_VISIBLE_COLUMNS));
+
+  const headerColumns = useMemo(() => {
+    if (visibleColumns === 'all') return columns;
+
+    return columns.filter((column) => Array.from(visibleColumns).includes(column.uid));
+  }, [visibleColumns]);
+
+  const renderCell = useCallback((applyData: ApplyEquipmentRequest, columnKey: React.Key) => {
+    const cellValue = applyData[columnKey as keyof ApplyEquipmentRequest];
+
+    switch (columnKey) {
+      case 'uid':
+      case 'eqId':
+      case 'applyTime':
+      case 'applyDate':
+        return <div>{cellValue}</div>;
+      case 'timeIndex':
+        return <div>{availableTime[cellValue]}</div>;
+      case 'status':
+        return <div>{cellValue}</div>;
+      case 'actions':
+        return (
+          <Popover
+            placement="bottom"
+            trigger="click"
+            content={
+              <PopoverContent>
+                <CheckboxGroup value={visibleColumns} onChange={setVisibleColumns}>
+                  {columns.map(({ name, uid }) => (
+                    <Checkbox key={uid} value={uid}>
+                      {name}
+                    </Checkbox>
+                  ))}
+                </CheckboxGroup>
+              </PopoverContent>
+            }
+          >
+            <PopoverTrigger>
+              <Button isIconOnly variant="flat">
+                <VerticalDotsIcon size={15} />
+              </Button>
+            </PopoverTrigger>
+          </Popover>
+        );
+      default:
+        return cellValue;
+    }
+  }, []);
+
   return (
-    <div className="wrapper">
-      <div className="toolbar"></div>
-      <Table>
-        <TableHeader>
-          <TableColumn>申请人</TableColumn>
-          <TableColumn>申请器材</TableColumn>
-          <TableColumn>使用日期</TableColumn>
-          <TableColumn>使用时段</TableColumn>
-          <TableColumn>申请时间</TableColumn>
-          <TableColumn>状态</TableColumn>
-          <TableColumn>操作</TableColumn>
-        </TableHeader>
-        <TableBody>
-          {mockApplyEquipmentRequests.map((request) => {
-            return (
-              <TableRow key={request.uid + request.eqId + request.applyDate + request.applyTime}>
-                <TableCell>{request.uid}</TableCell>
-                <TableCell>
-                  <Chip color="secondary">{request.eqId}</Chip>
-                </TableCell>
-                <TableCell>{request.applyDate}</TableCell>
-                <TableCell>{availableTime[parseInt(request.timeIndex)]}</TableCell>
-                <TableCell>{request.applyTime}</TableCell>
-                <TableCell>{request.status}</TableCell>
-                <TableCell>
-                  <Dropdown>
-                    <DropdownTrigger>
-                      <Button isIconOnly variant="light">
-                        <VerticalDotsIcon size={20} />
-                      </Button>
-                    </DropdownTrigger>
-                    <DropdownMenu>
-                      <DropdownItem key="view">查看</DropdownItem>
-                      <DropdownItem key="approve" color="success" className="text-success">
-                        通过
-                      </DropdownItem>
-                      <DropdownItem key="reject" color="danger" className=" text-danger">
-                        驳回
-                      </DropdownItem>
-                    </DropdownMenu>
-                  </Dropdown>
-                </TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </div>
+    <Table
+      aria-label="Example table with custom cells, pagination and sorting"
+      isHeaderSticky
+      topContentPlacement="outside"
+    >
+      <TableHeader columns={headerColumns}>
+        {(column) => (
+          <TableColumn
+            key={column.uid}
+            align={column.uid === 'actions' ? 'center' : 'start'}
+            allowsSorting={column.sortable}
+          >
+            {column.name}
+          </TableColumn>
+        )}
+      </TableHeader>
+      <TableBody emptyContent={'No users found'} items={mockApplyEquipmentRequests}>
+        {(item) => (
+          <TableRow key={`${item.uid}-${item.eqId}-${item.applyDate}-${item.timeIndex}`}>
+            {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
   );
 };
