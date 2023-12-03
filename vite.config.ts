cae9c1d4b28c040942340534fa406862d24de989
WitchElaina <msz13399173387@gmail.com>
2023-12-03 12:42:10 +0800

    Rel 1.0

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,18 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8820/',
+        changeOrigin: true,
+      },
+      '/file-api': {
+        target: 'http://127.0.0.1:8080/',
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
