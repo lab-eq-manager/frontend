1688225967dda9aa403f2d65a08380c11ca363d0
WitchElaina <msz13399173387@gmail.com>
2023-12-09 09:21:40 +0800

    Add new button for empty list

--- src/layouts/LabListView.tsx
+++ src/layouts/LabListView.tsx
@@ -3,9 +3,13 @@ import { LabTable } from '@/components/LabTable';
 import { getLabList } from '@/utils/requests';
 import { useToast } from '@/components/ui/use-toast';
 
+import { Button } from '@nextui-org/react';
+import { useNavigate } from 'react-router-dom';
+
 export const LabListView: React.FC = () => {
   const [tableData, setTableData] = React.useState();
   const { toast } = useToast();
+  const navigate = useNavigate();
 
   const getData = () => {
     getLabList()
@@ -28,8 +32,17 @@ export const LabListView: React.FC = () => {
   return (
     <div className="lab-list-wrapper flex flex-col items-center gap-8 w-full">
       <div className="title font-semibold text-2xl">实验室列表</div>
-      {tableData && tableData?.length !== 0 && (
+      {tableData && tableData?.length !== 0 ? (
         <LabTable tableData={tableData} canCustomColumn getData={() => getData()} />
+      ) : (
+        <Button
+          color="primary"
+          onClick={() => {
+            navigate(`/manage/lab/add`);
+          }}
+        >
+          添加实验室
+        </Button>
       )}
     </div>
   );
--- src/layouts/UserListView.tsx
+++ src/layouts/UserListView.tsx
@@ -2,8 +2,12 @@ import { UserTable } from '@/components/UserTable';
 import { getManageUserList } from '@/utils/requests';
 import { useEffect, useState } from 'react';
 
+import { Button } from '@nextui-org/react';
+import { useNavigate } from 'react-router-dom';
+
 export const UserListView = () => {
   const [tableData, setTableData] = useState();
+  const navigate = useNavigate();
 
   useEffect(() => {
     getManageUserList()
@@ -17,8 +21,17 @@ export const UserListView = () => {
     <div className="flex flex-col items-center gap-8 w-full">
       <div className="title font-semibold text-2xl">用户管理</div>
       <div className="w-full">
-        {tableData && tableData?.length !== 0 && (
+        {tableData && tableData?.length !== 0 ? (
           <UserTable tableData={tableData} canCustomColumn />
+        ) : (
+          <Button
+            color="primary"
+            onClick={() => {
+              navigate(`/manage/user/add`);
+            }}
+          >
+            添加用户
+          </Button>
         )}
       </div>
     </div>
